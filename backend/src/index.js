5652058accef2d2015ad8dfdd002828d7b480733
sagnik0b0957 <your-real-sagnik0b0957@gmail.com>
2026-09-06 14:10:27 +0530

    Add Dockerfile and .dockerignore; implement server static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 //const express = require('express');
 import express from "express";
 import cors from "cors"; // cors(cross-origin resource sharing) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the resource originated.
+
 import dotenv from "dotenv/config";
+
+import fs from "fs";
+import path from "path";
 
 import { clerkMiddleware } from "@clerk/express";
 
@@ -12,8 +16,11 @@ import dns from "dns";
 dns.setServers(["8.8.8.8"]);
 
 const app = express();
+
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(
@@ -28,6 +35,16 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+//if the public directory does not exist, serve static files from the public directory
+//this is for production build, when the frontend is built and served from the backend
+if (!fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get("/{*any}", (req, res, next) => {  
+    res.sendFile(path.join(publicDir,"index.html"), (error));
+  });
+}
+
 
 app.listen(PORT, () => {
   connectDB();

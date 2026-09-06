6b6a236d90342bbd83006f6166c9d39f40b7b469
sagnik0b0957 <your-real-sagnik0b0957@gmail.com>
2026-09-06 16:43:27 +0530

    Reorder import statements for consistency in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 //const express = require('express');
 import express from "express";
 import cors from "cors"; // cors(cross-origin resource sharing) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the resource originated.
-import job from "./lib/cron.js";
+
 
 import dotenv from "dotenv/config";
 
@@ -12,6 +12,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 import dns from "dns";
 dns.setServers(["8.8.8.8"]);
